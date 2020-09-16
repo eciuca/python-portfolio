c862335ef91bd62ff5f4f79f2f32a429c95962bf
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2020-09-16 19:16:46 +0300

    Move favicon

--- server.py
+++ server.py
@@ -11,3 +11,8 @@ def my_home():
 @app.route('/<string:page>')
 def static_pages(page='index.html'):
     return render_template(page)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def login():
+    return 'form submitted hooorraayyyy!'
