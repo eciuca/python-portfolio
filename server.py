65f54246499af592fea88bca0e581a3415495a97
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2020-09-16 19:33:57 +0300

    Save info to 'database'

--- server.py
+++ server.py
@@ -17,7 +17,15 @@ def static_pages(page='index.html'):
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
         return redirect('thankyou.html')
     else:
         return 'try again'
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
