e054b6dff52c663125db0a06390b78859ddffa36
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2020-09-16 19:12:17 +0300

    Add changes up to Potfolio 3

--- server.py
+++ server.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/<username>/<int:post_id>')
-def hello_world(username=None, post_id=None):
-    return render_template('./index.html', name=username, post_id=12)
 
-@app.route('/about')
-def about():
-    return render_template('./about.html')
+@app.route('/')
+def my_home():
+    return render_template('index.html')
 
-@app.route('/blog')
-def blog():
-    return {'message': 'These are my thoughts on blogs'}
 
-@app.route('/blog/2020/dogs')
-def blog2():
-    return 'These are my thoughts on blogs 2020'
+@app.route('/<string:page>')
+def static_pages(page='index.html'):
+    return render_template(page)
